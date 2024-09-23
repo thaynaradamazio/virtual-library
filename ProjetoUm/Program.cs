83c6935c70d4b70407e04bd5d5d3e17807ba5f00
Thaynara Damazio <thaydamazio2015@gmail.com>
2024-09-23 14:20:06 -0300

    Alterações na organização do código e adição do método para pesquisar livro pelo nome

--- ProjetoUm/Program.cs
+++ ProjetoUm/Program.cs
@@ -49,14 +49,11 @@ while (continuar)
 
  void AddJaLidos()
 {
-    Console.Write("Digite o nome do livro: ");
-    string name = Console.ReadLine();
+    string name = ReadLineWithText("Digite o nome do livro: ");
     
-    Console.Write("Digite o nome do autor: ");
-    string author = Console.ReadLine();
-    
-    Console.Write("Digite a sinopse do livro: ");
-    string synopsis = Console.ReadLine();
+    string author = ReadLineWithText("Digite o nome do autor: ");
+ 
+    string synopsis = ReadLineWithText("Digite a sinopse do livro: ");
 
     Console.Write("Digite a quantidade de páginas: ");
     int pages = int.Parse(Console.ReadLine());
@@ -76,20 +73,16 @@ while (continuar)
         context.SaveChanges(); 
     }
 
-    Console.WriteLine("Livro já lido adicionado com sucesso!");
-    Console.ReadLine();
+    ReadLineWithText("Livro já lido adicionado com sucesso!");
 }
 
  void AddQueroLer()
 {
-    Console.Write("Digite o nome do livro: ");
-    string name = Console.ReadLine();
+    string name = ReadLineWithText("Digite o nome do livro: ");
     
-    Console.Write("Digite o nome do autor: ");
-    string author = Console.ReadLine();
-    
-    Console.Write("Digite a sinopse do livro: ");
-    string synopsis = Console.ReadLine();
+    string author = ReadLineWithText("Digite o nome do autor: ");
+ 
+    string synopsis = ReadLineWithText("Digite a sinopse do livro: ");
 
     Console.Write("Digite a quantidade de páginas: ");
     int pages = int.Parse(Console.ReadLine());
@@ -110,95 +103,97 @@ while (continuar)
         context.SaveChanges(); 
     }
 
-    Console.WriteLine("Livro para ler adicionado com sucesso!");
-    Console.ReadLine();
+    ReadLineWithText("Livro para ler adicionado com sucesso!");
 }
 
   void ListarJaLidos()
   {
-
-      using (var context = new ApplicationDbContext())
+      using var context = new ApplicationDbContext();
+      
+      string bookName = ReadLineWithText("Digite o nome do livro a ser pesquisado (Para listar todos os livros tecle 'ENTER'): ");
+          
+      var lidos = context.Lido.ToList();
+      
+      if (lidos.Count == 0)
       {
-          var lidos = context.Lido.ToList();
-
-          if (lidos.Count == 0)
-          {
-              Console.WriteLine("Nenhum livro já lido cadastrado.");
-          }
-          
-          Console.WriteLine("LIVROS JÁ LIDOS");
-          foreach (var l in lidos)
-          {
-              Console.WriteLine($"Nome do livro: {l.Name}\nAutor: {l.Author}\nNúmero de páginas: {l.Pages}\nSinopse: {l.Synopsis}");
-              Console.WriteLine("--------------------");
-          }
-
-          Console.ReadLine();
-          
+          ReadLineWithText("Nenhum livro já lido cadastrado.");
+          return;
       }
+
+      if (string.IsNullOrEmpty(bookName) == false)
+      {
+          lidos = lidos.Where(book => book.Name == bookName).ToList();
+      }
+          
+      Console.WriteLine("LIVROS JÁ LIDOS");
+      Console.WriteLine("--------------------");
+      foreach (var l in lidos)
+      {
+          Console.WriteLine($"Nome do livro: {l.Name}\nAutor: {l.Author}\nNúmero de páginas: {l.Pages}\nSinopse: {l.Synopsis}");
+          Console.WriteLine("--------------------");
+      }
+
+      Console.ReadLine();
   }
   
   void ListarQueroLer()
   {
+      using var context = new ApplicationDbContext();
       
-      using (var context = new ApplicationDbContext())
-      {
-          var lerei = context.Lerei.ToList();
-
-          if (lerei.Count == 0)
-          {
-              Console.WriteLine("Nenhum livro para ler cadastrado.");
-          }
-
-          Console.WriteLine("LIVROS PARA LER");
-          foreach (var l in lerei)
-          {
-              Console.WriteLine($"Nome do livro: {l.Name}\nAutor: {l.Author}\nNúmero de páginas: {l.Pages}\nSinopse: {l.Synopsis}");
-              Console.WriteLine("--------------------");
-          }
+      string bookName = ReadLineWithText("Digite o nome do livro a ser pesquisado (Para listar todos os livros tecle 'ENTER'): ");
           
-          Console.ReadLine();
-      }
+      var lerei = context.Lerei.ToList();
       
+      if (lerei.Count == 0)
+      {
+          ReadLineWithText("Nenhum livro para ler cadastrado.");
+          return;
+      }
+
+      if (string.IsNullOrEmpty(bookName) == false)
+      {
+          lerei = lerei.Where(book => book.Name == bookName).ToList();
+      }
+          
+      Console.WriteLine("LIVROS PARA LER");
+      Console.WriteLine("--------------------");
+      foreach (var l in lerei)
+      {
+          Console.WriteLine($"Nome do livro: {l.Name}\nAutor: {l.Author}\nNúmero de páginas: {l.Pages}\nSinopse: {l.Synopsis}");
+          Console.WriteLine("--------------------");
+      }
+
+      Console.ReadLine();
   }
   
   void RemoverJaLidos()
   {
-      using (var connection = new ApplicationDbContext())
+      using var connection = new ApplicationDbContext();
+      
+      var bookName = ReadLineWithText("Digite o nome do livro: ");
+          
+      var bookAuthor = ReadLineWithText("Digite o nome do autor: ");
+          
+      var searchedBook = connection.Lido.FirstOrDefault(l => l.Name == bookName && l.Author == bookAuthor);
+      
+      if (searchedBook != null)
       {
-        Console.Write("Digite o nome do livro: ");
-        var bookName = Console.ReadLine(); 
-      
-        Console.Write("Digite o nome do autor do livro: ");
-        var bookAuthor = Console.ReadLine(); 
-      
-        var searchedBook = connection.Lido.FirstOrDefault(l => l.Name == bookName && l.Author == bookAuthor);
-      
-        if (searchedBook != null)
-        {
           connection.Lido.Remove(searchedBook);
           connection.SaveChanges();
 
-          Console.WriteLine("Livro removido");
-          Console.ReadLine();
+          ReadLineWithText("Livro removido.");
           return;
-        }
-        Console.WriteLine("Livro não encontrado");
-        Console.ReadLine();
-      
       }
-          
+      ReadLineWithText("Livro não encontrado.");
   }
 
   void RemoverQueroLer()
   {
       using var connection = new ApplicationDbContext();
-      
-      Console.Write("Digite o nome do livro: ");
-      var bookName = Console.ReadLine(); 
-      
-      Console.Write("Digite o nome do autor do livro: ");
-      var bookAuthor = Console.ReadLine(); 
+
+      var bookName = ReadLineWithText("Digite o nome do livro: ");
+
+      var bookAuthor = ReadLineWithText("Digite o nome do autor: ");
       
       var searchedBook = connection.Lerei.FirstOrDefault(l => l.Name == bookName && l.Author == bookAuthor);
       
@@ -207,52 +202,44 @@ while (continuar)
           connection.Lerei.Remove(searchedBook);
           connection.SaveChanges();
 
-          Console.WriteLine("Livro removido");
-          Console.ReadLine();
+          ReadLineWithText("Livro removido.");
           return;
       }
-      Console.WriteLine("Livro não encontrado");
-      Console.ReadLine();
+      ReadLineWithText("Livro não encontrado.");
   }
 
   void AtualizarJaLidos()
   {
       using var context = new ApplicationDbContext();
       
-      Console.Write("Digite o nome atual do livro: ");
-      var bookName = Console.ReadLine();
+      var bookName = ReadLineWithText("Digite o nome atual do livro: ");
           
       var book = context.Lido.FirstOrDefault(l => l.Name == bookName);
 
       if (book == null)
       {
-          Console.Write("Livro não encontrado");
-          return;
+          ReadLineWithText("Livro não encontrado.");
       }
           
-      Console.Write("Qual informação você deseja atualizar? Opções - nome, autor, sinopse ou páginas: ");
-      string info = Console.ReadLine();
+      string info = ReadLineWithText("Qual informação você deseja atualizar? Opções - nome, autor, sinopse ou páginas: ");
           
       switch (info)
       {
           case "nome":
           {
-              Console.Write("Digite o novo nome do livro: ");
-              var newBookName = Console.ReadLine();
+              var newBookName = ReadLineWithText("Digite o novo nome do livro: ");
               book.Name = newBookName;
               break;
           }
           case "autor":
           {
-              Console.Write("Digite o novo nome do autor do livro: ");
-              var newAuthorName = Console.ReadLine();
+              var newAuthorName = ReadLineWithText("Digite o novo nome do autor do livro: ");
               book.Author = newAuthorName;
               break;
           }
           case "sinopse":
           {
-              Console.Write("Digite a nova sinopse do livro: ");
-              var newSynopsis = Console.ReadLine();
+              var newSynopsis = ReadLineWithText("Digite a nova sinopse do livro: ");
               book.Synopsis = newSynopsis;
               break;
           }
@@ -265,8 +252,7 @@ while (continuar)
           } 
           default:
           {
-              Console.WriteLine("Opção inválida");
-              Console.ReadLine();
+              ReadLineWithText("Opção inválida.");
               return;
           }
       }
@@ -286,40 +272,34 @@ while (continuar)
   {
       using var context = new ApplicationDbContext();
       
-      Console.Write("Digite o nome atual do livro: ");
-      var bookName = Console.ReadLine();
+      var bookName = ReadLineWithText("Digite o nome atual do livro: ");
           
       var book = context.Lerei.FirstOrDefault(l => l.Name == bookName);
 
       if (book == null)
       {
-          Console.Write("Livro não encontrado");
-          return;
+          ReadLineWithText("Livro não encontrado.");
       }
           
-      Console.Write("Qual informação você deseja atualizar? Opções - nome, autor, sinopse ou páginas: ");
-      string info = Console.ReadLine();
+      string info = ReadLineWithText("Qual informação você deseja atualizar? Opções - nome, autor, sinopse ou páginas: ");
           
       switch (info)
       {
           case "nome":
           {
-              Console.Write("Digite o novo nome do livro: ");
-              var newBookName = Console.ReadLine();
+              var newBookName = ReadLineWithText("Digite o novo nome do livro: ");
               book.Name = newBookName;
               break;
           }
           case "autor":
           {
-              Console.Write("Digite o novo nome do autor do livro: ");
-              var newAuthorName = Console.ReadLine();
+              var newAuthorName = ReadLineWithText("Digite o novo nome do autor do livro: ");
               book.Author = newAuthorName;
               break;
           }
           case "sinopse":
           {
-              Console.Write("Digite a nova sinopse do livro: ");
-              var newSynopsis = Console.ReadLine();
+              var newSynopsis = ReadLineWithText("Digite a nova sinopse do livro: ");
               book.Synopsis = newSynopsis;
               break;
           }
@@ -332,8 +312,7 @@ while (continuar)
           } 
           default:
           {
-              Console.WriteLine("Opção inválida");
-              Console.ReadLine();
+              ReadLineWithText("Opção inválida.");
               return;
           }
       }
@@ -348,6 +327,10 @@ while (continuar)
       Console.WriteLine($"{infoUpper} alterado com sucesso.");
       Console.ReadLine();
   }
-  
-  
+
+    string ReadLineWithText(string text) 
+    { 
+        Console.WriteLine(text); 
+        return Console.ReadLine();
+    }  
   
